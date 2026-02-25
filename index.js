9cdd7091313f0806936ca91a252c8a0a5e53c5a1
Mubashirijaz786 <mubashirejaz786@gmail.com>
2026-02-25 13:11:26 +0500

    FINALLLL

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(helmet({
 
 app.use(cors({ 
     origin: process.env.FRONTEND_URL || "http://localhost:5173", 
+    origin: allowedOrigin,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
